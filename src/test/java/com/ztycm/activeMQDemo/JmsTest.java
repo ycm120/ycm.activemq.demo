c2b8e41ec1759ede780653163ab7996494a6442e
ycm120 <18710615070@163.com>
2019-06-02 15:40:11 +0800

    test

--- src/test/java/com/ztycm/activeMQDemo/JmsTest.java
+++ src/test/java/com/ztycm/activeMQDemo/JmsTest.java
@@ -5,8 +5,6 @@ import java.util.Map;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.scheduling.annotation.EnableAsync;
